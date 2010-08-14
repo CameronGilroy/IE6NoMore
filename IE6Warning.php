7af78dc9437d0a8d521977278bc5a09e89a9cfea
Cameron Gilroy <git@ripple7.com>
2010-08-15 08:03:30 +1000

    Removed a blank line

--- IE6Warning.php
+++ IE6Warning.php
@@ -6,7 +6,6 @@ Place in the header of the page so a to be able to give a warning to IE6 users w
 */
 	$userAgent = strtolower($_SERVER["HTTP_USER_AGENT"]);
 	if ( eregi("msie 6", $userAgent) || eregi("msie 5", $userAgent) ) {
-	  echo "<script src='http://static.turnwheel.com/ie6/warning.js'></script><script>window.onload=function(){_ie6('http://static.turnwheel.com/ie6/')}</script>";
-	  
+	  echo "<script src='http://static.turnwheel.com/ie6/warning.js'></script><script>window.onload=function(){_ie6('http://static.turnwheel.com/ie6/')}</script>"; 
 	}
 ?>
