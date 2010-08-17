e357d210cd2afd16d5624c6444561def6282389a
Cameron Gilroy <git@ripple7.com>
2010-08-17 17:43:36 +1000

    Just inserted the readme into the snippet

--- IE6NoMore.php
+++ IE6NoMore.php
@@ -1,8 +1,29 @@
 <?php
 /*
-Example: [[!IE6NoMore]]
 
-Place in the header of the page so a to be able to give a warning to IE6 users with out having ugly "<!--[if lt IE 7]><![endif]-->" tags in the head!
+h1. IE6NoMore
+
+h2. About
+
+* By Cameron Gilroy
+* IE6NoMore
+* Version: 1.0
+* Creator: Cameron Gilroy http://www.camerongilroy.com
+* Support URL: http://www.camerongilroy.com/
+* GitHub: http://github.com/camgill/IE6NoMore
+
+The aim of this plugin is to help remind users of IE6 that they need to upgrade their browser!
+
+By Cameron Gilroy
+
+h3. Usage
+
+Place [[!IE6NoMore]] just after the first @<body>@ tag to give a warning to IE6 users without having ugly @<!--[if lt IE 7]><![endif]-->@ tags in your html code!
+
+You can also call the contents of your own chunk like this @[[!IE6NoMore? &Message=`[[$Your_Chunk_Name]]`]]@
+
+You can also use a string of text like @[[!IE6NoMore? &Message=`<h1>Go Away IE6!!</h1>`]]@
+
 */
 $Message = isset($Message)? $Message : '[[$IE6NoMore]]';
 
