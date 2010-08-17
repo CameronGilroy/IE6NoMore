248916f3d4403cb0023b21adca9427b12476c50c
Cameron Gilroy <git@ripple7.com>
2010-08-17 15:38:02 +1000

    Just a name change

--- IE6NoMore.php
+++ IE6NoMore.php
@@ -1,11 +1,13 @@
 <?php
 /*
-Example: [[!IE6Warning]]
+Example: [[!IE6NoMore]]
 
 Place in the header of the page so a to be able to give a warning to IE6 users with out having ugly "<!--[if lt IE 7]><![endif]-->" tags in the head!
 */
+$Message = isset($Message)? $Message : '[[$IE6NoMore]]';
+
 	$userAgent = strtolower($_SERVER["HTTP_USER_AGENT"]);
 	if ( eregi("msie 6", $userAgent) || eregi("msie 5", $userAgent) ) {
-	  echo "<script src='http://static.turnwheel.com/ie6/warning.js'></script><script>window.onload=function(){_ie6('http://static.turnwheel.com/ie6/')}</script>"; 
+	  echo "$Message"; 
 	}
 ?>
